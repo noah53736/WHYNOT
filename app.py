011aec4bc7487b3c2d8b21d18b4ae3fbf1b42c96
noah53736 <83874189+noah53736@users.noreply.github.com>
2025-01-26 22:31:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import string
 from datetime import datetime
 import json
 import matplotlib.pyplot as plt
-import subprocess  # Ajout pour la fonction accelerate_ffmpeg
+import subprocess  # Pour l'accélération audio
 
 from pydub import AudioSegment, silence
 
